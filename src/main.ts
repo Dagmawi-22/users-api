8b4101e6e9c80ff422c42a2cad3f10ab293327f6
unknown <dagtt316@gmail.com>
2024-03-12 14:16:39 +0300

    feat: add comments to make things clear & provide example env

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,13 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // setup swagger documentation
   const config = new DocumentBuilder()
     .setTitle('Users API')
     .setDescription('API documentation for Users API')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
