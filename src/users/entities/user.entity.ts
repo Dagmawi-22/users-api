b2b31b1cef58f4ac125e07557f91d988b77cbe99
unknown <dagtt316@gmail.com>
2024-03-12 14:20:56 +0300

    feat: make email & phone unique

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Model, Table } from 'sequelize-typescript';
+import { Column, Model, Table, Unique } from 'sequelize-typescript';
 
 @Table
 export class User extends Model<User> {
@@ -9,9 +9,11 @@ export class User extends Model<User> {
   @Column
   name: string;
 
+  @Unique
   @Column
   email: string;
 
+  @Unique
   @Column
   phone: string;
 }
